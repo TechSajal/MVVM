8d3554c19ddb92ed346c8ea72fb83cf82da0ab7d
sajal <sajalkaushal3@gmail.com>
2022-08-01 23:41:01 +0530

    "chore":- mvvm using dummy data done

--- app/src/main/java/View/MainActivity.kt
+++ app/src/main/java/View/MainActivity.kt
@@ -1,13 +1,47 @@
 package View
 
+import ViewModel.ListViewModel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.mvvm.R
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewModel: ListViewModel
+    private val countriesadapter = CountryListAdapter(arrayListOf())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+      viewModel = ViewModelProviders.of(this)[ListViewModel::class.java]
+        viewModel.refresh()
+        findViewById<RecyclerView>(R.id.countriesList).apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = countriesadapter
+        }
+        observeviewmodel()
+    }
 
+    private fun observeviewmodel() {
+        viewModel.Countries.observe(this, Observer{countries ->
+            countries?.let { countriesadapter.updateCountries(it) }
+        })
+        viewModel.CountryLoadError.observe(this,Observer{iserror ->
+            iserror?.let { findViewById<TextView>(R.id.list_error).visibility =if(it) View.VISIBLE else View.GONE}
+        })
+        viewModel.Loading.observe(this,Observer{isloading ->
+            isloading?.let {
+                findViewById<ProgressBar>(R.id.loading_view).visibility = if (it)View.VISIBLE else View.GONE
+                if (it){
+                    findViewById<TextView>(R.id.list_error).visibility = View.GONE
+                    findViewById<RecyclerView>(R.id.countriesList).visibility = View.GONE
+                }
+            }
+        })
     }
 }
--- app/src/main/java/ViewModel/ListViewModel.kt
+++ app/src/main/java/ViewModel/ListViewModel.kt
@@ -1,7 +1,39 @@
 package ViewModel
 
+import Model.Country
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ListViewModel:ViewModel(){
+   val Countries = MutableLiveData<List<Country>>()
+    val CountryLoadError = MutableLiveData<Boolean>()
+    val Loading = MutableLiveData<Boolean>()
+    fun refresh(){
+        fetchCountries()
+    }
+
+    private fun fetchCountries() {
+        val mockdata = listOf(Country("CountryA"),
+            Country("CountryB"),
+            Country("CountryC"),
+            Country("CountryD"),
+            Country("CountryE"),
+            Country("CountryF"),
+            Country("CountryG"),
+            Country("CountryH"),
+            Country("CountryI"),
+            Country("CountryJ"),
+            Country("CountryK"),
+            Country("CountryL"),
+            Country("CountryM"),
+            Country("CountryN"),
+            Country("CountryO"),
+            Country("CountryP"),
+            Country("CountryQ"),
+            Country("CountryR"))
+        Countries.value = mockdata
+        Loading.value=false
+        CountryLoadError.value = false
+    }
 
 }
