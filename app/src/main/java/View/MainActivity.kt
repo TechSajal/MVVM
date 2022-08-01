a03d2f4285406b3af05002953ce611d80920a23b
sajal <sajalkaushal3@gmail.com>
2022-08-01 21:49:57 +0530

    "chore":- dependencies Done

---
+++ app/src/main/java/View/MainActivity.kt
@@ -0,0 +1,13 @@
+package View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mvvm.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
